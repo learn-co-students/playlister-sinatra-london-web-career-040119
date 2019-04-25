66a0bc32f7d3275129de4222e105677eda2a9f0c
github username <github email address>
2019-04-25 18:06:44 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Artist.destroy_all
+Song.destroy_all
+Album.destroy_all
+SongGenre.destroy_all
+Genre.destroy_all
 
+LibraryParser.parse
--- lib/library_parser.rb
+++ lib/library_parser.rb
@@ -34,7 +34,8 @@ class LibraryParser
 
     song.song_genres.build(genre: genre)
     song.artist = artist
-    
+    artist.albums.build(genre: genre)
+    artist.save
     song.save
   end
 end
