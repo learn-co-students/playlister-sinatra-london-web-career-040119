662191cc19fc27b6b8fb055aeeac456f4f8eb53d
github username <github email address>
2019-04-26 15:09:08 +0100

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,10 +2,12 @@ class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
+    # binding.pry
     erb :"songs/index"
   end
 
   get '/songs/new' do
+
     erb :"songs/new"
   end
 
@@ -16,11 +18,30 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    @song = Song.create(name: params["song"]["name"])
-    Artist.find_or_create_by(name: params["song"]["artist"]["name"])
-
-    Artist.find_or_create_by(name: params["song"]["genre"]["name"])
-
-    @song.save
+    artist = Artist.find_or_create_by(name: params["artist_name"])
+    genre = Genre.find_or_create_by(name: params["genre_name"])
+    song = Song.create(name: params["song_name"], artist_id: artist.id)
+    song.song_genres.build(genre: genre)
+    song.save
+    redirect "songs/#{song.slug}"
   end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :"songs/edit"
+  end
+
+  patch '/songs/:slug' do
+    song = Song.find_by_slug(params[:slug])
+    song.update(name: params["song"]["name"])
+    song.artist.name = params["song"]["artist"]
+    song.genres.update(name: params["song"]["genre"])
+    redirect "songs/#{song.slug}"
+  end
+
+  get '/songs/' do
+    redirect "/songs"
+  end
+
+
 end
